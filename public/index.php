46ad881143c4942a97bff31807e84b1dca29b5cd
EmilioMM <emarmor346@g.educaand.es>
2024-12-09 15:32:57 +0100

    Cambio en la clase /public/index.php

--- public/index.php
+++ public/index.php
@@ -1,4 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 require_once '../autoload.php';
+require $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . '../vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . DIRECTORY_SEPARATOR . "..");
+$dotenv->load();
+
+session_start();
+
 require_once '../routes/web.php';
+
+// require_once $_SERVER["DOCUMENT_ROOT"] . DIRECTORY_SEPARATOR . '../routes' . DIRECTORY_SEPARATOR . 'web.php';
